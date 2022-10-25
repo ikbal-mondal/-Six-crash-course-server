d08a526c62f702249aa8c5519572428cc3b918d5
ikbal-mondal <kbalmondal062@gmail.com>
2022-10-25 19:30:25 +0530

    added id

--- index.js
+++ index.js
@@ -5,15 +5,28 @@ app.use(cors());
 
 const Port = process.env.Port || 5000;
 
-const AllCourseCollection = require("./Data/Course.json");
+const AllCourse = require("./Data/Course.json");
  const categories = require('./data/Category.json')
 app.get("/", (req, res) => {
   res.send("Now server is running");
 });
 
 app.get("/allCourse", (req, res) => {
-  res.send(AllCourseCollection);
+  res.send(AllCourse);
 });
+
+app.get('/AllCourse/:id', (req, res) => {
+  const id = req.params.id
+  const selectCourse = AllCourse.find(course => course._id == id);
+  res.send(selectCourse)
+})
+
+app.get('/category/:id', (req, res) => {
+
+  const categoryId = req.params.id;
+  const category_Course = AllCourse.find(course => course.category_id == categoryId) ;
+  res.send(category_Course)
+})
 
 app.get('/course-categories', (req,res) => {
 
