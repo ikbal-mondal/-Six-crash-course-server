2a3ee718c9a39917e054d86ac7c00b06ca73bca7
ikbal-mondal <kbalmondal062@gmail.com>
2022-10-27 13:05:36 +0530

    create vercel json file

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ app.use(cors());
 const Port = process.env.Port || 5000;
 
 const AllCourse = require("./Data/Course.json");
- const categories = require('./data/Category.json')
+const categories = require("./data/Category.json");
 app.get("/", (req, res) => {
   res.send("Now server is running");
 });
@@ -15,25 +15,23 @@ app.get("/allCourse", (req, res) => {
   res.send(AllCourse);
 });
 
-app.get('/AllCourse/:id', (req, res) => {
-  const id = req.params.id
-  const selectCourse = AllCourse.find(course => course._id == id);
-  res.send(selectCourse)
-})
+app.get("/AllCourse/:id", (req, res) => {
+  const id = req.params.id;
+  const selectCourse = AllCourse.find((course) => course._id == id);
+  res.send(selectCourse);
+});
 
-app.get('/category/:id', (req, res) => {
-
+app.get("/category/:id", (req, res) => {
   const categoryId = req.params.id;
-  const category_Course = AllCourse.find(course => course.category_id == categoryId) ;
-  res.send(category_Course)
-})
+  const category_Course = AllCourse.find(
+    (course) => course.category_id == categoryId
+  );
+  res.send(category_Course);
+});
 
-app.get('/course-categories', (req,res) => {
-
-  res.send(categories)
-})
-
-
+app.get("/course-categories", (req, res) => {
+  res.send(categories);
+});
 
 app.listen(Port, () => {
   console.log("server is running", Port);
